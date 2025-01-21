5da662c85af02cd5ca92b7f7495703218214a92a
muthukumar <rlm.muthukumar@gmail.com>
2025-01-21 20:15:03 +0530

    Single Number Returns Its Value

--- string_calculator.py
+++ string_calculator.py
@@ -3,3 +3,9 @@ def add(numbers):
     if not numbers:
         return 0
     return None  # Placeholder for future steps
+
+def add_number(numbers):
+    if not numbers:
+        return 0
+    return int(numbers)
+ 
--- stringcal.py
+++ stringcal.py
@@ -1,10 +1,15 @@
 import unittest
-from string_calculator import add
+from string_calculator import add,add_number 
 
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
     
+    def test_single_number(self):
+        self.assertEqual(add_number("5"), 5)
+
+     
+
 if __name__ == "__main__":
     unittest.main()
 
